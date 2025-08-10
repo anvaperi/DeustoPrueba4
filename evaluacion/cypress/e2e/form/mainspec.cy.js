f599fd0978b9ac958f3b5ba89b4f7e5f880131e9
anvaperi <donantonio5314@gmail.com>
2025-08-10 21:20:57 +0200

    mainspec.cy.js edited

--- evaluacion/cypress/e2e/form/mainspec.cy.js
+++ evaluacion/cypress/e2e/form/mainspec.cy.js
@@ -6,3 +6,13 @@ describe('Formulario usuarios', () => {
     cy.get('[for="repeat-password"]').contains('Repita contraseña');
   })
 })
+
+describe('Formulario usuarios', () => {
+  it('Advertencia y botón invisible para contraseñas discordantes', () => {
+    cy.visit('http://127.0.0.1:5500/evaluacion/index.html');
+    cy.get('#password').type('abc');
+    cy.get('#repeat-password').type('def');
+    cy.get('#send-btn').should('have.css', 'display', 'none');
+    cy.get('#password-message').should('have.css', 'display', 'block');
+  })
+})
