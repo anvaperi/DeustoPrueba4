7fc4c87415b7b55d6d19a187cf2784b78fc932bf
anvaperi <donantonio5314@gmail.com>
2025-08-10 20:14:03 +0200

    reinstalled Cypress

--- evaluacion/cypress/support/integration/form/main.spec.js
+++
@@ -1,8 +0,0 @@
-describe('Formulario usuarios', () => {
-  it('Contiene los campos email, contraseña y repetición de contraseña', () => {
-    cy.visit('http://127.0.0.1:5500/index.html');
-    cy.get('[for="email"]').contains('Email');
-    cy.get('[for="password"]').contains('Contraseña');
-    cy.get('[for="repeat-password"]').contains('Repetición de contraseña');
-  })
-})
