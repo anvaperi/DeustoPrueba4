fbd149762e1c354f928929c44ad238d91bc88d2b
anvaperi <donantonio5314@gmail.com>
2025-08-10 20:36:50 +0200

    index.html and mainspec.cy.js updated

--- evaluacion/cypress/e2e/form/mainspec.cy.js
+++ evaluacion/cypress/e2e/form/mainspec.cy.js
@@ -1,8 +1,8 @@
 describe('Formulario usuarios', () => {
   it('Contiene los campos email, contraseña y repetición de contraseña', () => {
-    cy.visit('http://127.0.0.1:5500/index.html');
-    cy.get('[for="email"]').contains('Email');
+    cy.visit('http://127.0.0.1:5500/evaluacion/index.html');
+    cy.get('[for="email"]').contains('Correo electrónico');
     cy.get('[for="password"]').contains('Contraseña');
-    cy.get('[for="repeat-password"]').contains('Repetición de contraseña');
+    cy.get('[for="repeat-password"]').contains('Repita contraseña');
   })
 })
